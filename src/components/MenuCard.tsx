c429e36a3c2d43cf43f95d20af2cda73ccb547c0
DennisVN <78357110+DennisVN@users.noreply.github.com>
2021-10-11 14:28:17 +0200

    Remove standard React tab

--- src/components/MenuCard.tsx
+++ src/components/MenuCard.tsx
@@ -6,7 +6,6 @@ export interface IAppProps {
 export default function App (props: IAppProps) {
   return (
     <div>
-      
     </div>
   );
 }
--- src/components/Reviews.tsx
+++ src/components/Reviews.tsx
@@ -5,8 +5,6 @@ export interface IAppProps {
 
 export default function App (props: IAppProps) {
   return (
-    <div>
-      
-    </div>
+    <div></div>
   );
 }
