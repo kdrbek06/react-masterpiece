b0aab7fde17b8cfabc8267a88c7854d822c1c43e
Kadir  Bek <kdrbek@gmail.com>
2021-10-13 15:58:27 +0200

    Create navbar with tailwindcss

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -5,8 +5,23 @@ export interface IAppProps {
 
 export default function App (props: IAppProps) {
   return (
-    <div>
-      
-    </div>
+    <nav className="flex item-center justify-between flex-wrap bg-red-800 p-6">
+      <div className="flex item-center flex-shrink-0 text-white mr-6">
+        <span className="font-bold text-xl">Brand Name</span>
+      </div>
+
+      <div className="w-full block flex-grow sm:flex sm:items-center sm:w-auto">
+        <ul className="text-sm sm:flex-grow">
+          <li><a href="Menus" className="block mt-4 sm:inline-block sm:mt-0 text-teal-200 hover:text-white mr-4"></a></li>
+          <li><a href="Reviews" className="block mt-4 sm:inline-block sm:mt-0 text-teal-200 hover:text-white mr-4"></a></li>
+          <li><a href="Collabration" className="block mt-4 sm:inline-block sm:mt-0 text-teal-200 hover:text-white mr-4"></a></li>
+          <li><a href="Contact" className="block mt-4 sm:inline-block sm:mt-0 text-teal-200 hover:text-white mr-4"></a></li>
+        </ul>
+      </div>
+      <div>
+        <a href="" className="inline-block text-sm px-4 py-2 leading-none border rounded text-white border-white hover:border-transparent hover:text-blue-500 hover:bg-white mt-4 md:mt-0">Login</a>
+      </div>
+
+    </nav>
   );
 }
--- src/components/ReviewCard.tsx
+++ src/components/ReviewCard.tsx
@@ -4,7 +4,7 @@ export default function ReviewCard() {
   return (
     <div className="p-6 max-w-sm mx-auto bg-white rounded-xl shadow-md flex items-center space-x-4">
       <div className="flex-shrink-0">
-        <img className="h-12 w-12" src="/img/logo.svg" alt="ChitChat Logo">
+        <img className="h-12 w-12" src="/img/logo.svg" alt="ChitChat Logo"></img>
       </div>
       <div>
         <div className="text-xl font-medium text-black">ChitChat</div>
