7d0706a6d3602c7aeda09f0fb3b6d34777cd1ac2
DennisVN <78357110+DennisVN@users.noreply.github.com>
2021-10-13 16:02:03 +0200

    Accept changes for merging

--- src/components/ReviewCard.tsx
+++ src/components/ReviewCard.tsx
@@ -4,11 +4,11 @@ export default function ReviewCard() {
   return (
     <div className="p-6 max-w-sm mx-auto bg-white rounded-xl shadow-md flex items-center space-x-4">
       <div className="flex-shrink-0">
-        <img className="h-12 w-12" src="/img/logo.svg" alt="ChitChat Logo">
+        <img className="h-12 w-12" src="/img/logo.svg" alt="ChitChat Logo"></img>
       </div>
       <div>
         <div className="text-xl font-medium text-black">ChitChat</div>
-        <p className="text-gray-500">You have a new message!</p>
+        <p className="text-blue-500">You have a new message!</p>
       </div>
     </div>
   )
