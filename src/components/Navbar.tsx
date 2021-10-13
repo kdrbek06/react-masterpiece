ccc41aa82625a3a0266b9e1dc12c1aa935bdb4c3
Kadir  Bek <kdrbek@gmail.com>
2021-10-13 16:26:18 +0200

    Change navbar style

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -5,17 +5,17 @@ export interface IAppProps {
 
 export default function App (props: IAppProps) {
   return (
-    <nav className="flex item-center justify-between flex-wrap bg-red-800 p-6">
+    <nav className="flex item-center justify-between flex-wrap bg-gray-600 p-6">
       <div className="flex item-center flex-shrink-0 text-white mr-6">
         <span className="font-bold text-xl">Brand Name</span>
       </div>
 
       <div className="w-full block flex-grow sm:flex sm:items-center sm:w-auto">
-        <ul className="text-sm sm:flex-grow">
-          <li><a href="Menus" className="block mt-4 sm:inline-block sm:mt-0 text-teal-200 hover:text-white mr-4"></a></li>
-          <li><a href="Reviews" className="block mt-4 sm:inline-block sm:mt-0 text-teal-200 hover:text-white mr-4"></a></li>
-          <li><a href="Collabration" className="block mt-4 sm:inline-block sm:mt-0 text-teal-200 hover:text-white mr-4"></a></li>
-          <li><a href="Contact" className="block mt-4 sm:inline-block sm:mt-0 text-teal-200 hover:text-white mr-4"></a></li>
+        <ul className="text-sm sm:flex">
+          <li><a href="/" className="block mt-4 sm:inline-block sm:mt-0 text-teal-200 hover:text-white mr-4">Menus</a></li>
+          <li><a href="/" className="block mt-4 sm:inline-block sm:mt-0 text-teal-200 hover:text-white mr-4">Reviews</a></li>
+          <li><a href="/" className="block mt-4 sm:inline-block sm:mt-0 text-teal-200 hover:text-white mr-4">Collabration</a></li>
+          <li><a href="/" className="block mt-4 sm:inline-block sm:mt-0 text-teal-200 hover:text-white mr-4">Contact</a></li>
         </ul>
       </div>
       <div>
