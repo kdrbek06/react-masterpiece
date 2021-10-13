426c7f8c281b54c20e1e8406bb25d6f7093e21c7
DennisVN <78357110+DennisVN@users.noreply.github.com>
2021-10-13 11:00:41 +0200

    Install and implemet React Router Dom

--- src/App.tsx
+++ src/App.tsx
@@ -7,21 +7,11 @@ import Navbar from '../src/components/Navbar'
 
 function App() {
   return (
-<<<<<<< HEAD
-    <p>test</p>
-=======
     <div className="App">
-      
 
       <Navbar/>
 
-
-
-
-
-
     </div>
->>>>>>> 852dac3a5c59ad898c0913f796664b8c88f59544
   );
 }
 
--- src/components/ReviewCard.tsx
+++ src/components/ReviewCard.tsx
@@ -1,10 +1,15 @@
-import * as React from 'react';
+import React from 'react'
 
-export interface IAppProps {
-}
-
-export default function App (props: IAppProps) {
+export default function ReviewCard() {
   return (
-    <div></div>
-  );
+    <div className="p-6 max-w-sm mx-auto bg-white rounded-xl shadow-md flex items-center space-x-4">
+      <div className="flex-shrink-0">
+        <img className="h-12 w-12" src="/img/logo.svg" alt="ChitChat Logo">
+      </div>
+      <div>
+        <div className="text-xl font-medium text-black">ChitChat</div>
+        <p className="text-gray-500">You have a new message!</p>
+      </div>
+    </div>
+  )
 }
