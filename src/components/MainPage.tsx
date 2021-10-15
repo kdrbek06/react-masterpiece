d75891316bcb8d36dca0a48ca41fabcb99175f24
Kadir  Bek <kdrbek@gmail.com>
2021-10-15 02:09:30 +0200

    Move mainCarousel to mainpage

--- src/components/MainPage.tsx
+++ src/components/MainPage.tsx
@@ -1,8 +1,10 @@
 import React from 'react'
+import MainCarousel from '../components/MainCarousel';
 
 export default function MainPage() {
     return (
         <div>
+            <MainCarousel />
             <p>this is the Main Page Mayne</p>
         </div>
     )
