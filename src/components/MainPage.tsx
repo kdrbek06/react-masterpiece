eafa3cb4159eb07ecd6775076a062def2da598bb
Kadir  Bek <kdrbek@gmail.com>
2021-10-22 10:25:28 +0200

    Add aboutus section and some text

--- src/components/MainPage.tsx
+++ src/components/MainPage.tsx
@@ -1,15 +1,15 @@
 import React from 'react'
 import MainCarousel from '../components/MainCarousel';
+import AboutUs from './AboutUs';
 import Cooks from '../components/Cooks'
+import Collaboration from './Collaboration';
+import ReviewCard from './ReviewCard';
 
 import Cook1 from '../image/cook1.jpg';
 import Cook2 from '../image/cook2.jpg';
 import Cook3 from '../image/cook3.jpg';
 import Cook4 from '../image/cook4.jpg';
-import ReviewCard from './ReviewCard';
-import Collaboration from './Collaboration';
 import Coll1 from '../image/coll1.png'
-
 
 
 
@@ -17,24 +17,24 @@ const reviews = [
     {
         image: Coll1,
         name: "Piet U.",
-        comment: "Great food, would order again",
+        comment: "Great food, would order again. Lorem, ipsum dolor sit amet consectetur adipisicing elit",
         date: "02/01/21",
     },
     {
         image: Coll1,
         name: "Piet U.",
-        comment: "Great food, would order again",
+        comment: "consectetur adipisicing elit. Porro, adipisci ratione.Great food, would order again",
         date: "02/01/21",
     }, {
         image: Coll1,
         name: "Piet U.",
-        comment: "Great food, would order again",
+        comment: "Great food, would order again. Lorem, ipsum dolor sit amet consectetur adipisicing elit",
         date: "02/01/21",
     },
     {
         image: Coll1,
         name: "Piet U.",
-        comment: "Great food, would order again",
+        comment: "adipisicing elit. Porro, adipisci ratione.Great food, would order again",
         date: "02/01/21",
     }
 ];
@@ -71,6 +71,8 @@ export default function MainPage() {
         <div>
 
             <MainCarousel />
+            <AboutUs />
+             
 
 
 
@@ -83,7 +85,7 @@ export default function MainPage() {
 
 
 
-            <div className="text-center bg-gray-300 p-8">
+            <div className="text-center p-8">
                 <h2 className="my-8 text-3xl font-bold" >OUR CLIENTS LOVE US</h2>
                 <div className="flex flex-wrap text-center items-center justify-center">
                     {reviews.map((review) => <ReviewCard {...review} />)}
