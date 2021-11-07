3993d9e92a9414f063a5a0c025f5c6de27ea47b0
DennisVN <78357110+DennisVN@users.noreply.github.com>
2021-11-07 22:52:50 +0100

    Leave it for today. Updated graphic UX, WIP align prices to the right

--- src/components/Menus.tsx
+++ src/components/Menus.tsx
@@ -9,14 +9,19 @@ export default function Menus() {
 
   return (
     <>
-    <div className="p-4 border-4 bg-yellow-200 bg-opacity-70 py-6 px-16 text-opacity-50 text-6xl text-black">Menu Card</div>
-      <div className="border-solid border-4 border-light-blue-500">
-      {Object.values(menus).map((menu, id) => (
-        <div key={id}>
-          <MenuCard menu={menu} />
-          <div className="text-lg font-semibold"><AddForm theMenu={menu}/></div>
+    <div className="md:container md:mx-auto mx-auto h-800 w-900 p-4 px-4">
+      <div className="s p-4 border-4">
+        <div className="box-border md:box-content h-350 w-800 text-center decoration-clone my-6 p-6 sm:mx6 border-solid border-4 border-light-blue-500">Menu Card
         </div>
-      ))}
+        <div className="grid grid-cols-3 grid-template-columns: repeat(3, minmax(0, 1fr))">
+          {Object.values(menus).map((menu, id) => (
+            <div key={id}>
+              <MenuCard menu={menu} />
+              <div className="text-lg font-semibold"><AddForm theMenu={menu}/></div>
+            </div>
+          ))}
+          </div>
+      </div>
     </div>
     </>
   )
