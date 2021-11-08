c069bc9bac3763c55c1a42eda5acf35019ba2b61
DennisVN <78357110+DennisVN@users.noreply.github.com>
2021-11-08 16:13:28 +0100

    other obligations, WIP make modal responsive, Where did the login menu header go ?

--- src/components/MenuCard.tsx
+++ src/components/MenuCard.tsx
@@ -8,23 +8,23 @@ export default function MenuCard({menu}) {
   
   return (
     <>
-      <div className=" text-lg font-semibold bg-gray-200">Day : 
+      <div className="text-lg font-semibold bg-gray-800 text-yellow-400 font-rancho"> 
         <div className="flex text-xl font-light">{menu.day}</div>
       </div>
-      <div  className=" text-lg font-semibold">Entrée :
+      <div  className="text-lg font-semibold">Entrée :
         <div className="flex text-xl font-light">{menu.nameStarter}</div>
-        <div className="grid justify-items-start bg-yellow-200"><div>€{menu.priceStarter}</div></div>
+        <div className="grid justify-items-end text-yellow-400 font-rancho text-2xl"><div>€{menu.priceStarter}</div></div>
       </div>
       <div className="text-lg font-semibold">Main Dish : 
         <div className="flex text-xl font-light">{menu.nameMain}</div>
-        <div className=" bg-yellow-200">€{menu.priceMain}</div>
+        <div className="grid justify-items-end text-yellow-400 font-rancho text-2xl">€{menu.priceMain}</div>
       </div>
       <div className=" text-lg font-semibold">Dessert :
         <div className="flex text-xl font-light">{menu.nameDessert}</div>
-        <div className="flex bg-yellow-200">€{menu.priceDessert}</div>
+        <div className="grid justify-items-end text-yellow-400 font-rancho text-2xl">€{menu.priceDessert}</div>
       </div>
       <div className=" text-lg font-semibold">Total Price :
-        <div className="flex bg-green-200">€ {menu.totalPrice}</div>
+        <div className="flex justify-items-end text-green-400 font-rancho text-2xl">€ {menu.totalPrice}</div>
       </div>
     </>
   )
