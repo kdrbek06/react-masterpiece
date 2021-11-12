51cb4bfa9b90c5bc3c60b59e9f2b028e49c745d1
DennisVN <78357110+DennisVN@users.noreply.github.com>
2021-11-12 02:20:13 +0100

    Update Menu Text

--- src/components/MenuOverview.tsx
+++ src/components/MenuOverview.tsx
@@ -13,7 +13,7 @@ export default function MenuOverview() {
       <div className="md:flex justify-evenly items-center">
         <p className="max-w-xl text-base tracking-wider">
         <h2 className="modifiedH2 max-w-xl mx-auto text-4xl text-center px-auto py-auto opacity-80%" >This Week's Menu</h2>
-        Welcome to the Menu Page. Here you find the overview of this weeks selection ! All dishes are carefully selected and prepared by our cooks. Click the Yes Please button to add the selected day to the card. And you know what they say, Tippin is not a county in China ;).
+        Welcome to the Menu Page. Here you find the overview of this weeks selection ! All dishes are carefully selected and prepared by our cooks. 
         Here at Real Meal we highly value our planet, and stand strongly against the wase of plastic and food. Therefore we disabled the option of individual orders, and you can only choose to select the complete Menu per day. 
         This is also a fair system for our cooks, as we also value their time and effort. <br/><br/>
         Keep a close eye on this page, as our Cooks have the ability to constantly update their Menu. They have absolute freedom over when and what they offer on the Menu. Scroll down if you want to see this week's Menu Card, and start ordering delicious local meals, prepared with love by our Cooks ! 
