1f9c75d257e1dac8701ea1bb29cc2bb6f4afb94f
DennisVN <78357110+DennisVN@users.noreply.github.com>
2021-11-12 14:02:59 +0100

    Update story on HomePage

--- src/components/AboutUs.tsx
+++ src/components/AboutUs.tsx
@@ -2,6 +2,7 @@ import * as React from 'react';
 import aboutImg from '../image/food3.png';
 import aboutImg2 from '../image/food5.png';
 import aboutImg3 from '../image/food2.jpg';
+import { Link } from 'react-router-dom';
 
 export interface IAppProps {
 }
@@ -10,17 +11,22 @@ export default function App(props: IAppProps) {
   return (
     <div id="about" className="m-2 p-2 lg:m-16 text-center">
       <h2 className="modifiedH2" >About Us</h2>
-      <p className="m-auto md:w-2/3 mb-8">In our journey that started with restaurant-quality home-cooked takeaway service in 2021, we opened 34 takeaway branches in Ghent in a short time and reached the level of 1,500 online orders per day.
+      <p className="m-auto md:w-2/3 mb-8"> Welcome to the Real Meal Website ! Let's introduce ourselves : <br/><br/>
+        
+        <b>Real Meal provides restaurant-quality home-cooked takeaway</b> service since 2021. Our Cooks are your local people who love to cook, and offer their weekly menu on our site. 
 
-        In 7 months between, we tried to constantly improve ourselves and diversify our services. We have always advanced our food quality, product variety and technologies we use.
+        The biggest distinction between us and other food sharing platforms, is that we keep it P2P, instead of working with the traditional oversaturated multinational restaurants.
 
-        In 2021, we launched our Packaged Corporate Hot Lunch Subscription service. With our corporate catering service, which does not require kitchen equipment and does not require a service person, we have started to offer gourmet dining experience to employees by reducing the catering costs of companies.</p>
+        We highly incentivize our Cooks to reduce plastic and avoid the waste of food. One of our initiatives here is that you can't order single dishes, as you can only order per Daily Menu.
+         
+        Here at Real Meal we say <b>fresh, local, personal</b> and <b>environmentally conscious</b>.  </p>
 
       <div className="md:flex justify-evenly items-center p-8">
-        <p className="max-w-xl">Kitchens and Cooks gives further intimation usual menu, stating confidently that "Boulanger served salted poultry and fresh eggs, all presented without a tablecloth on small marble tables". Numerous commentators have also referred to the supposed restaurant owner's eccentric habit of touting for custom outside his establishment, dressed in aristocratic fashion and brandishing a sword
+        <p className="max-w-xl">This is how it works : If you want to order from the menu, go have a look at our <br/> <Link to="/menu" className="text-pink-700">Menu's Page </Link>.
 
-          According to Miss Spang, there is not a shred of evidence for any of it. She said: These legends just get passed on by hearsay and then spiral out of control. Her interest in Boulanger dates back to a history of food seminar in Paris in the mid-1990s
+          If you are a passionate preparer of delicious meals, why not <Link to="/Login" className="text-pink-700">log in </Link> and start sharing your custom made Menu . It's a nice way to earn some extra money as well, as we minimize the profit we make on the transactions . We vouch for fair prices, and an honest income for our Cooks. <br/>
 
+          Transparency is key at Real Meal. We take our client's feedback very serious. We base our future cooperations with Cooks on the received reviews. 
         </p>
         <img src={aboutImg} className="w-full md:w-1/3 transform transition duration-500 hover:scale-110" alt="" />
       </div>
@@ -28,13 +34,15 @@ export default function App(props: IAppProps) {
       <div className="md:flex justify-evenly items-center p-8">
         <img src={aboutImg2} className="w-full md:w-1/3 transform transition duration-500 hover:scale-110" alt="" />
         <p className="max-w-xl">
-          The History of Kitchens and Cooks gives further intimation on Mr Boulanger usual menu, stating confidently that "Boulanger served salted poultry and fresh eggs, all presented without a tablecloth on small marble tables". Numerous commentators have also referred to the supposed restaurant owner's eccentric habit of touting for custom outside his establishment, dressed in aristocratic fashion and brandishing a sword
+          Real Meal is an initiative of two aspiring developers, Kadir and Dennis, who want to bring local communities together through the forgotten art of cooking. We live in a fastpaced world, and have forgotten the golden days where friends and family came together and enjoy a freshly cooked meal after a hard day of work. 
 
-          According to Miss Spang, there is not a shred of evidence for any of it. She said: These legends just get passed on by hearsay and then spiral out of control. Her interest in Boulanger dates back to a history of food seminar in Paris in the mid-1990s</p>
+          We don't offer junkfood, and don't work with mass production cuisine. A true Real Meal Cook is expected to use fresh and local products, be it from their own garden or local (super)market .          
+          
+          </p>
       </div>
 
       <div className="md:flex justify-evenly items-center p-8">
-        <p className="max-w-xl">Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolore, in. Assumenda, iste mollitia, quas error nulla cum alias voluptas quia ullam quis, harum non! Dolorem veniam fuga ipsa. Quos odio aperiam ab nesciunt a quae voluptate, facilis vero mollitia suscipit ad tempora quod, temporibus inventore neque dolorum excepturi dignissimos doloremque!</p>
+        <p className="max-w-xl">Real Meal is a Belgian based website with HQ's in Ghent and and Antwerp. We are always looking for (local) partnerships, and collaboration in order to grow and improve our service to the community. We prefer valuable colobarotion over lobby sponsorships.           Make sure to contact us if you want to collaborate or have a great idea / initiative. We are here to bring people together through food and passion for cooking, so the possibilities are infinite !</p>
         <img src={aboutImg3} className="w-full md:w-1/3 transform transition duration-500 hover:scale-110" alt="" />
       </div>
 
