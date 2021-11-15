8024eca30eb3d169ca05ae1780ab5992255b9b89
Kadir  Bek <kdrbek@gmail.com>
2021-11-15 10:01:42 +0100

    Change text in review tsx

--- src/components/Reviews.tsx
+++ src/components/Reviews.tsx
@@ -9,7 +9,7 @@ export default function Reviews() {
           <div className="w-full max-w-6xl mx-auto">
             <div className="text-center max-w-xl mx-auto">
               <h1 className="text-6xl md:text-6xl font-bold mb-5 text-gray-600">What people <br />  are saying.</h1>
-              <h3 className="text-xl mb-5 font-light">Lorem ipsum dolor sit amet consectetur adipisicing elit.</h3>
+              <h3 className="text-xl mb-5 font-light">You can see valuable comments from our customers here. Customer satisfaction means everything to us</h3>
               <div className="text-center mb-10">
                 <span className="inline-block w-1 h-1 rounded-full bg-darkkhaki ml-1"></span>
                 <span className="inline-block w-3 h-1 rounded-full bg-darkkhaki ml-1"></span>
